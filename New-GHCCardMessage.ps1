334400de934caa6ef615c3465467539d0ac5e1b1
Tom E. Pomeroy <pomeroyte@mansfieldct.org>
2018-03-20 09:00:54 -0400

    Initial Commit

--- New-GHCCardMessage.ps1
+++ New-GHCCardMessage.ps1
@@ -20,3 +20,15 @@ function New-GHCCardMessage {
 }
 
 
+
+
+New-GHCCardMessage -Endpoint {
+    New-GHCCardHeader -Title "This is a header" -subtitle "This is the sub title" -imageURL "http:\\www.img.com\img.png" -imageStyle "avatar"
+    New-GHCCardSection -Endpoint {
+        New-GHCCardSectionHeader -Text "Section"
+        New-GHCCardWidget -Endpoint {
+            New-GHCCardWidgetImage
+            New-GHCCardWidgetKeyValue
+        }
+    }
+}
